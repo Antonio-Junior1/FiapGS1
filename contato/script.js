14d3c3baed13810c58ba7d0d53a62334c8ebce39
Mateus-Pierro <168029936+Mateus077777@users.noreply.github.com>
2024-06-07 08:18:39 -0300

    Update and rename contato.js to script.js

---
+++ contato/script.js
@@ -0,0 +1,10 @@
+function enviarChamado() {
+    alert("Chamado enviado com sucesso!");
+    window.location.href = "/FRONT PAGE/index.html"; 
+    return false; 
+}
+
+function resgatarPremio() {
+    alert("Prêmio resgatado com sucesso!");
+    window.location.href = "/FRONT PAGE/index.html"; 
+}
